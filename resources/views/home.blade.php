de227bb2d08a1b4b0eddfa800a0bfd56c283a9d4
José Manuel Corona Villán <ChemaCorona>
2019-02-05 08:42:29 +0100

    Primera entrega

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Balbe</title>
+	</head>
+	<body>
+		@extends('layouts.master')
+
+		@section('content')
+			Pantalla principal
+		@stop
+	</body>
+</html>
